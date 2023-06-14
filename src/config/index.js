9679a0f55514f047b612d551827506ecbc659082
jay bhogayata <jaybhogayata53@gmail.com>
2023-06-14 09:12:40 +0530

    feat: add some user related method's

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,8 @@ dotenv.config();
 const config = {
   PORT: process.env.PORT || 3000,
   MONGODB_URI: process.env.MONGODB_URI || "mongodb://127.0.0.1/stream_db",
+  JWT_SECRET: process.env.JWT_SECRET || "change_this_please",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "7d",
 };
 
 export default config;
--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import SubscriptionType from "../utils/SubTypes.js";
 import AuthRoles from "../utils/AuthRoles.js";
+import config from "../config/index.js";
+import { createHash, randomBytes } from "node:crypto";
 
 const userSchema = new Schema(
   {
@@ -39,7 +43,7 @@ const userSchema = new Schema(
       type: Boolean,
       default: false,
     },
-    otp: Number,
+    otp: String,
     otpExpiry: Date,
     forgotPasswordToken: String,
     forgotPasswordExpiry: Date,
@@ -48,6 +52,46 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  return next();
+});
+
+userSchema.methods = {
+  comparePassword: async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  },
+
+  getJWTToken: function () {
+    return jwt.sign({ _id: this.id, role: this.role }, config.JWT_SECRET, {
+      expiresIn: config.JWT_EXPIRY,
+    });
+  },
+
+  generateForgotPasswordToken: function () {
+    const forgotToken = randomBytes(20).toString("hex");
+
+    this.forgotPasswordToken = createHash("sha256")
+      .update(forgotToken)
+      .digest("hex");
+
+    this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000;
+
+    return forgotToken;
+  },
+
+  generateOtp: function () {
+    const otp = randomBytes(6).toString("hex");
+
+    this.otp = createHash("sha256").update(otp).digest("hex");
+
+    this.otpExpiry = Date.now() + 10 * 60 * 1000;
+
+    return otp;
+  },
+};
 
 const User = mongoose.model("User", userSchema);
 
