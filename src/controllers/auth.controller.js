5d10f834e9ccb4a06c554551a421515ff6a310a0
jay bhogayata <jaybhogayata53@gmail.com>
2023-06-16 00:23:37 +0530

    feat - add login and logout routes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,6 +4,11 @@ import CustomError from "../utils/CustomError.js";
 import mailHelper from "../utils/mailHelper.js";
 import { createHash } from "node:crypto";
 
+export const cookieOptions = {
+  expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+  httpOnly: true,
+};
+
 export const sendSignUpOtp = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -63,7 +68,42 @@ export const signup = asyncHandler(async (req, res) => {
       message: `user is verified with email ${user.email}`,
     });
   } else {
-    throw new CustomError("either otp is wrong or otp time expires.", 400);
     await user.deleteOne();
+    throw new CustomError("either otp is wrong or otp time expires.", 400);
   }
+});
+
+export const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    throw new CustomError("user does not exists", 400);
+  }
+
+  const isPasswordMatch = await user.comparePassword(password);
+
+  if (isPasswordMatch) {
+    const token = user.getJWTToken();
+    user.password = undefined;
+    res.cookie("token", token, cookieOptions);
+    return res.status(200).json({
+      success: true,
+      token,
+      user,
+    });
+  }
+  throw new CustomError("password is incorrect", 400);
+});
+
+export const logout = asyncHandler(async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "user logged out successfully.",
+  });
 });
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { sendSignUpOtp, signup } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  sendSignUpOtp,
+  signup,
+} from "../controllers/auth.controller.js";
 
 const router = new Router();
 
 router.post("/signupOtpSend", sendSignUpOtp);
 router.post("/signup", signup);
+router.post("/login", login);
+router.get("/logout", logout);
 
 export default router;
